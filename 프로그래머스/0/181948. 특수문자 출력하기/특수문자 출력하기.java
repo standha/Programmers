0b9cf8aba7a5e832ecdb9677004ca011eb2379e0
SeoHa <90242821+standha@users.noreply.github.com>
2024-07-10 15:34:39 +0900

    [level 0] Title: 특수문자 출력하기, Time: 134.35 ms, Memory: 61.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+       // \를 사용하여 백슬래시()를 출력, '는 홑따옴표(')를 출력하기 위해 사용되며, \"는 쌍따옴표(")를 출력하기 위해 사용 
+        
+        System.out.print("!@#$%^&*(\\'\"<>?:;");
+        
+    }
+}
