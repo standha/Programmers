5159151a618244e7a5d0d36dfb0a28f5116113c2
SeoHa <90242821+standha@users.noreply.github.com>
2025-04-28 14:43:12 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.03 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+        answer = new int[num_list.length - n+1];
+        int k = 0;
+        for(int i = n-1; i<num_list.length; i++){
+            answer[k] = num_list[i];
+            k++;
+        }
+        return answer;
+    }
+}
