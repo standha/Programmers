3a6307dc194843f1c3b460a878b62b51eccb569e
SeoHa <90242821+standha@users.noreply.github.com>
2025-05-10 14:27:01 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.13 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -1,10 +1,8 @@
 class Solution {
     public String[] solution(String my_string) {
         
-        //String[] answer = my_string.split(" ");
-        
-        String[] answer = {};
-        answer = my_string.split(" ");
+        String[] answer = my_string.split(" ");
+
         return answer;
     }
 }
