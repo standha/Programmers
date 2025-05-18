54942a8cacebee4ba598b02cefd91df87806e0d1
SeoHa <90242821+standha@users.noreply.github.com>
2025-05-18 16:45:35 +0900

    [level 0] Title: 주사위의 개수, Time: 0.02 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int[] box, int n) {
+//         int answer = 0;
+//         answer = (box[0]/n)*(box[1]/n)*(box[2]/n); //가로*세로*높이
+        
+        return (box[0]/n)*(box[1]/n)*(box[2]/n); //가로*세로*높이
+    }
+}
